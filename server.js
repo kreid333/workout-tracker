4145c613921d5684eac9cd447f798f99f53ae54d
kreid333 <kai.j.reid@gmail.com>
2020-10-22 16:30:52 -0400

    created folder structure as well as server and database connections

---
+++ server.js
@@ -0,0 +1,35 @@
+// DEFINING VARIABLES
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+// DEFINING MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// CONNECTING TO MONGODB
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// CREATING A VISIBLE NOTICE THAT WE HAVE CONNECTED TO THE DB
+const connection = mongoose.connection;
+
+connection.on("connected", function () {
+  console.log("Mongoose connected...");
+});
+
+// LISTENING ON SERVER
+app.listen(PORT, function () {
+  console.log(`App listening on http://localhost:${PORT}`);
+});
